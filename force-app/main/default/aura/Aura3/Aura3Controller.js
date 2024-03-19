0f7e29d54ee390a9bae5e91df006fd8b20a03888
Udayyyyy <udayradadiya65@gmail.com>
2024-03-19 11:35:45 +0530

    Update code

--- force-app/main/default/aura/Aura3/Aura3Controller.js
+++ force-app/main/default/aura/Aura3/Aura3Controller.js
@@ -1,15 +1,16 @@
 ({
     searchAccounts1: function(component, event, helper) {
         var searchText = component.find("search1").get("v.value"); 
-        var action = component.get("c.searchaccountcontact");
+        var action = component.get("c.searchAccounts");
         action.setParams({ searchkey: searchText });
         action.setCallback(this, function(response) {
             var state = response.getState();
             if (state === "SUCCESS") {
-                component.set('v.contactsInSection1', response.getReturnValue());
+                component.set('v.accountsInSection1', response.getReturnValue());
+                component.set('v.contactsInSection1', []);  
                 component.set("v.count1", response.getReturnValue().length);
             } else if (state === "ERROR") {
-                // Handle errors
+                console.log("error");
             }
         });
         $A.enqueueAction(action);
@@ -17,29 +18,45 @@
     
     searchAccounts2: function(component, event, helper) {
         var searchText = component.find("search2").get("v.value");
-        var action = component.get("c.searchaccountcontact");
+        var action = component.get("c.searchAccounts");
         action.setParams({ searchkey: searchText });
         action.setCallback(this, function(response) {
             var state = response.getState();
             if (state === "SUCCESS") {
-                component.set('v.contactsInSection2', response.getReturnValue());
+                component.set('v.accountsInSection2', response.getReturnValue());
+                component.set('v.contactsInSection2', []);  
                 component.set("v.count2", response.getReturnValue().length);
             } else if (state === "ERROR") {
-                // Handle errors
+                console.log("error");
             }
         });
         $A.enqueueAction(action);
     },
 
+    showContacts1: function(component, event, helper) {
+        var accountId = event.currentTarget.dataset.accountid;
+        component.set('v.selectedAccountId1', accountId);
+        
+    console.log('Selected Account ID for Section 1: ' + accountId);
+        helper.fetchContacts(component, accountId, 'section1');
+    },
+
+    showContacts2: function(component, event, helper) {
+        var accountId = event.currentTarget.dataset.accountid;
+        component.set('v.selectedAccountId2', accountId); 
+
+    console.log('Selected Account ID for Section 1: ' + accountId);
+        helper.fetchContacts(component, accountId, 'section2');
+    },
+
     dragStart: function(component, event, helper) {
-    var contactId = event.target.dataset.contactid;
-    var section = event.target.closest('[data-section]').getAttribute('data-section');
-    event.dataTransfer.setData("contactId", contactId);
-    event.dataTransfer.setData("section", section);
+        var contactId = event.target.dataset.contactid;
+        var section = event.target.closest('[data-section]').getAttribute('data-section');
+        event.dataTransfer.setData("contactId", contactId);
+        event.dataTransfer.setData("section", section);
     },
 
     drop: function(component, event, helper) {
-
         event.preventDefault();
         var contactId = event.dataTransfer.getData("contactId"); 
         var targetSection = event.target.closest('[data-section]').getAttribute('data-section');
@@ -47,39 +64,38 @@
         if (contactId && targetSection && sourceSection && targetSection !== sourceSection) {
             var accountId = '';   
             if (targetSection === 'section1') {
-              
-                accountId = component.get("v.contactsInSection1")[0].AccountId; 
-                
+                accountId = component.get("v.selectedAccountId1");
             } else if (targetSection === 'section2') {
-                accountId = component.get("v.contactsInSection2")[0].AccountId;  
-                 
+                accountId = component.get("v.selectedAccountId2");
             } 
             var updateAction = component.get("c.updateContactAccountId");
             updateAction.setParams({
                 contactId: contactId,
                 accountId: accountId
             });
-            
 
             updateAction.setCallback(this, function(response) {
+ 
+                  helper.fetchContacts(component, component.get('v.selectedAccountId1'), 'section1');
+                  helper.fetchContacts(component, component.get('v.selectedAccountId2'), 'section2');
                 var state = response.getState();
+                console.log(state);  
                 if (state === "SUCCESS") {
                     component.set("v.contactsInSection1", response.getReturnValue().section1Contacts);
                     component.set("v.contactsInSection2", response.getReturnValue().section2Contacts);
-                    this.searchAccounts1(component, event, helper);
-                    this.searchAccounts2(component, event, helper);
-
+                    
                     alert("success");
                 } else if (state === "ERROR") {
                     console.log('error');
                     alert('error');
-                 }
+                }
             });
             $A.enqueueAction(updateAction);
         }
     },
 
     dragover: function(component, event, helper) {
-        event.preventDefault(); 
+        event.preventDefault();   
+        helper.callSearchFunctions(component);
     }
 })
--- force-app/main/default/aura/Aura3/Aura3Helper.js
+++ force-app/main/default/aura/Aura3/Aura3Helper.js
@@ -1,4 +1,71 @@
 ({
-   
- 
+
+    
+    searchAccounts1: function(component, event, helper) {
+        var searchText = component.find("search1").get("v.value"); 
+        var action = component.get("c.searchaccountcontact");
+        action.setParams({ searchkey: searchText });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set('v.contactsInSection1', response.getReturnValue());
+                component.set("v.count1", response.getReturnValue().length);
+            } else if (state === "ERROR") {
+                 console.log("error");
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    
+    searchAccounts2: function(component, event, helper) {
+        var searchText = component.find("search2").get("v.value");
+        var action = component.get("c.searchaccountcontact");
+        action.setParams({ searchkey: searchText });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set('v.contactsInSection2', response.getReturnValue());
+                component.set("v.count2", response.getReturnValue().length);
+            } else if (state === "ERROR") {
+                 console.log("error");
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    showContacts: function(component, event, helper) {
+        var accountId = event.currentTarget.dataset.accountid;
+        var section = event.currentTarget.closest('[data-section]').getAttribute('data-section');
+        if (section === 'section1') {
+            component.set('v.selectedAccountId1', accountId);
+        } else if (section === 'section2') {
+            component.set('v.selectedAccountId2', accountId);
+        }
+        helper.fetchContacts(component, accountId, section);
+    },
+
+    fetchContacts: function(component, accountId, section) {
+        var action = component.get("c.getContactsByAccountId");
+        action.setParams({ accountId: accountId });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                if (section === 'section1') {
+                    component.set('v.contactsInSection1', response.getReturnValue());
+                    component.set("v.count1", response.getReturnValue().length);
+                } else if (section === 'section2') {
+                    component.set('v.contactsInSection2', response.getReturnValue());
+                    component.set("v.count2", response.getReturnValue().length);
+                }
+            } else if (state === "ERROR") {
+                 console.log("error");
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    callSearchFunctions: function(component) {
+        this.searchAccounts1(component);
+        this.searchAccounts2(component);
+    }
 })
